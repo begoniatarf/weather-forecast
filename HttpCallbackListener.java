c13ced377c252ac4bf908997afeaf6628ae6c02e
begoniatarf <179978466@qq.com>
2018-05-23 20:41:37 +0800

    Add files via upload

---
+++ HttpCallbackListener.java
@@ -0,0 +1,11 @@
+package com.example.administrator.weather;
+
+/**
+ * Created by Administrator on 2017/7/28 0028.
+ */
+
+public interface HttpCallbackListener {
+
+    void onFinish(String response);
+    void onError(Exception e);
+}
